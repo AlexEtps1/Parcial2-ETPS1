0227924922bc874ef40f1a058f8d791d864cb7bb
ErickAvlia23 <2511482019@mail.utec.edu.sv>
2023-03-22 22:02:57 -0600

    Inicio

--- Parcial22511482019/app/src/main/java/sv/edu/utec/parcial22511482019/MainActivity.java
+++ Parcial22511482019/app/src/main/java/sv/edu/utec/parcial22511482019/MainActivity.java
@@ -3,7 +3,13 @@ package sv.edu.utec.parcial22511482019;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -20,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
             R.drawable.lead_photo_7,
             R.drawable.lead_photo_8,
             R.drawable.lead_photo_9
-    }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,10 +39,37 @@ public class MainActivity extends AppCompatActivity {
         listaempleados.add(new Empleados("Sara Bonz", "Directora de Marketing", "Electricals Parts Itd"));
         listaempleados.add(new Empleados("Liliana Clarence", "Diseñadora de Producto", "Creative App"));
         listaempleados.add(new Empleados("Benito Peralta", "Supervisor de Ventas", "Neumaticos Press"));
-        listaempleados.add(new Empleados("Juan Jaramillo", "CEO", "Insures S.O."));
-        listaempleados.add(new Empleados("Christian Steps", "CTO", "Insures S.O."));
-        listaempleados.add(new Empleados("Alexa Giraldo", "Lead Programmer", "Insures S.O."));
-        listaempleados.add(new Empleados("Linda Murillo", "Directora de Marketing", "Insures S.O."));
-        listaempleados.add(new Empleados("Lizeth Astrada", "CEO", "Insures S.O."));
+        listaempleados.add(new Empleados("Juan Jaramillo", "CEO", "Banco Nacional"));
+        listaempleados.add(new Empleados("Christian Steps", "CTO", "Cooperativa Verde"));
+        listaempleados.add(new Empleados("Alexa Giraldo", "Lead Programmer", "Frutisofy"));
+        listaempleados.add(new Empleados("Linda Murillo", "Directora de Marketing", "Seguros Boliver"));
+        listaempleados.add(new Empleados("Lizeth Astrada", "CEO", "Concesionario Motolox"));
+    }
+
+    class AdapatdorEmpleados extends ArrayAdapter<Empleados>{
+        AppCompatActivity appCompatActivity;
+
+        AdapatdorEmpleados(AppCompatActivity context){
+            super(context, R.layout.empleados, listaempleados);
+            appCompatActivity=context;
+        }
+        public View getView(int position, View convertView, ViewGroup parent){
+            LayoutInflater inflater= appCompatActivity.getLayoutInflater();
+            View item= inflater.inflate(R.layout.empleados, null);
+
+            TextView tv1= item.findViewById(R.id.tvNombre);
+            tv1.setText(listaempleados.get(position).getNombre());
+
+            TextView tv2= item.findViewById(R.id.tvCargo);
+            tv2.setText(listaempleados.get(position).getCargo());
+
+            TextView tv3= item.findViewById(R.id.tvCompania);
+            tv3.setText(listaempleados.get(position).getCompania());
+
+            ImageView img1= item.findViewById(R.id.imvFoto);
+            img1.setImageResource(ImgEempleados[position]);
+
+            return item;
+        }
     }
 }
